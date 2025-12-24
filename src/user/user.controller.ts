6c2d25900d697a787e6829615b944269f905ca4a
FaribMojahirJonak <fmjnk248@gmail.com>
2025-12-24 16:28:29 +0600

    all http methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 
